7fd0453ed0fd22024cf868210810f813f0d5f7da
Rishabh Dwivedi <rd14121999@gmail.com>
2023-12-25 19:05:31 +0530

    Created local video streaming server

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
+app.use("/videos", express.static("videos"));
 
 app.get("/", (req, res) => {
-  res.send("hello ji");
+  const videosFolder = "videos";
+  fs.readdir(videosFolder, (err, files) => {
+    if (err) {
+      console.error("Error reading video files:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const videoNames = files.filter((file) => file.endsWith(".mp4"));
+    res.json({ videoNames });
+  });
+});
+
+app.get("/:videoName", (req, res) => {
+  const videoName = req.params.videoName;
+  const videoPath = `videos/${videoName}`;
+  res.sendFile(videoPath, { root: __dirname });
 });
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+module.exports = app;
