c4cc4c04b2c64b562d0e2d6cf595a24fa06e7597
Rishabh Dwivedi <rd14121999@gmail.com>
2024-01-02 14:22:29 +0530

    Implemented MongoDB

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
 app.use("/videos", express.static("videos"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  const videosFolder = path.join(__dirname, "videos");
-  fs.readdir(videosFolder, (err, files) => {
-    if (err) {
-      console.error("Error reading video files:", err);
-      return res.status(500).send("Internal Server Error");
-    }
+const videosSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  thumbnail: String,
+  video: String,
+});
 
-    const videoNames = files.filter((file) => file.endsWith(".mp4"));
-    res.json({ videoNames });
-  });
+const Videos = mongoose.model("Videos", videosSchema);
+
+mongoose.connect(
+  "mongodb+srv://rishabh14121999:rishabh@cluster0.y9lndvq.mongodb.net//videos",
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "videos" }
+);
+
+app.get("/api/videos", async (req, res) => {
+  try {
+    const videoData = await Videos.find();
+    res.json(videoData);
+  } catch (error) {
+    console.error("Error fetching video data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/:videoName", (req, res) => {
@@ -24,8 +37,20 @@ app.get("/:videoName", (req, res) => {
   res.sendFile(videoPath, { root: __dirname });
 });
 
+app.post("/api/upload", async (req, res) => {
+  try {
+    const { id, title, thumbnail, video } = req.body;
+    const newVideo = new Videos({ id, title, thumbnail, video });
+    await newVideo.save();
+    res.status(201).json({ message: "Video data uploaded successfully" });
+  } catch (error) {
+    console.error("Error uploading video data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server is running");
+  console.log("Server is running");
 });
 
 module.exports = app;
