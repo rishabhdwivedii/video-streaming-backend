0b023ba0c3ab91a1547a73989e9458e39596f3b7
Rishabh Dwivedi <rd14121999@gmail.com>
2024-01-21 23:11:50 +0530

    Updated auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -23,7 +23,7 @@ const authenticateJwt = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(" ")[1];
 
-    jwt.verify(token, secretKey, (err, user) => {
+    jwt.verify(token, SECRET, (err, user) => {
       if (err) {
         return res.sendStatus(403);
       }
