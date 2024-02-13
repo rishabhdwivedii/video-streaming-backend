ce38e197411acf12078e44e3d4a4d05444d2178a
Rishabh Dwivedi <rd14121999@gmail.com>
2024-02-14 00:47:54 +0530

    Added cors origin

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const appRouter = require("./routes/router");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://video-streaming-frontend-rho.vercel.app",
+  })
+);
 app.use(express.json());
 
 app.use("/", appRouter);
